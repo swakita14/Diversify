02aa318a212381a9e062ed6da8b00ef13be79ca9
Shion Wakita <swakita14@wou.edu>
2020-10-08 14:25:55 -0700

    added new page and service for the stock search function

--- Diversify_Server/Service/StockApiService.cs
+++ Diversify_Server/Service/StockApiService.cs
@@ -1,11 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Diversify_Server.Models;
+using Microsoft.Extensions.Configuration;
 
 namespace Diversify_Server.Service
 {
-    public class StockApiService
+    public class StockApiService 
     {
 
     }
