59668f6b4df6d00741cdf35bdc8f317282b30d99
Shion Wakita <swakita14@wou.edu>
2020-10-17 21:35:56 -0700

    need to rework form submission

---
+++ Diversify_Server/Models/StockNewsSearchModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diversify_Server.Models
+{
+    public class StockNewsSearchModel
+    {
+        public string CompanyName { get; set; }
+    }
+}
