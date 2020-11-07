791f4a9ba844f35dcf9ec779304c2fa51229d71c
Shion Wakita <swakita14@wou.edu>
2020-11-06 20:05:49 -0800

    working on datatable implementation

--- Diversify_Server/wwwroot/js/site.js
+++ Diversify_Server/wwwroot/js/site.js
@@ -1,11 +1,8 @@
-﻿//function saveMessage() {
-//    alert("Hello World");
-//}
+﻿
 
-//function organizeTable() {
-//    $(document).ready(function () {
-//        var table = $('#stockTable').DataTable();
-//    });
-
-//    alert("Hello World");
-//}
+function DataTables() {
+    $('#stockTable').load(function () {
+        $('#stockTable').DataTable();
+        alert("Hello World");
+    });
+}
