4723bc7fba3c470a4d31e7b35f89b4a749a99362
Shion Wakita <swakita14@wou.edu>
2020-12-20 16:56:10 -1000

    fixed file structure

--- Diversify_Server/Program.cs
+++ Diversify_Server/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using ElectronNET.API;
 
 namespace Diversify_Server
 {
@@ -22,6 +23,9 @@ namespace Diversify_Server
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
+                    webBuilder.UseElectron(args);
+                    webBuilder.UseEnvironment("Development");
+
                     webBuilder.UseStartup<Startup>();
                 });
     }
