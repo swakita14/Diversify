5eb00083ed0e50a39041eaa4c84fe61e62218a9d
Shion Wakita <swakita14@wou.edu>
2021-01-15 13:16:34 -0800

    injected initial services

---
+++ Diversify_Server/Controller/DiversifyWebAssembly/CompanyController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Diversify_Server.Interfaces.Services;
+
+namespace Diversify_Server.Controller.DiversifyWebAssembly
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CompanyController : ControllerBase
+    {
+        private readonly ICompanyService _companyService;
+
+        public CompanyController(ICompanyService companyService)
+        {
+            _companyService = companyService;
+        }
+    }
+}
---
+++ Diversify_Server/Controller/DiversifyWebAssembly/StockController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Diversify_Server.Interfaces.Services;
+using Diversify_Server.Services;
+
+namespace Diversify_Server.Controller.DiversifyWebAssembly
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StockController : ControllerBase
+    {
+        private readonly IStockService _stockService;
+
+        public StockController(IStockService stockService)
+        {
+            _stockService = stockService;
+        }
+    }
+}
